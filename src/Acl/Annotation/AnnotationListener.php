b3528518db1677393fcd57977460d4aa925bcbe7
Alex Oleshkevich <alex.oleshkevich@muehlemann-popp.ch>
2016-04-08 14:31:34 +0300

    sup. em 3

--- src/Acl/Annotation/AnnotationListener.php
+++ src/Acl/Annotation/AnnotationListener.php
@@ -17,7 +17,7 @@ class AnnotationListener extends AbstractListenerAggregate
      */
     protected $controllers = array();
 
-    public function attach(EventManagerInterface $events)
+    public function attach(EventManagerInterface $events, $priority = 1)
     {
         $this->listeners[] = $events->attach(ParseEvent::EVENT_CLASS_PARSED, [$this, 'onClassParsed']);
     }
