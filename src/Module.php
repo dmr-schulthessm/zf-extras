fa3d4cbf36d6e44144a9ce35d7a714bb2e03fe08
Alex Oleshkevich <alex.oleshkevich@muehlemann-popp.ch>
2016-04-13 18:14:26 +0300

    add BC 5.6

--- src/Module.php
+++ src/Module.php
@@ -4,31 +4,11 @@ namespace ZfExtra;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
-use Zend\ModuleManager\Listener\ServiceListenerInterface;
-use Zend\ModuleManager\ModuleManager;
 use Zend\Mvc\MvcEvent;
-use Zend\ServiceManager\ServiceLocatorInterface;
-use ZfExtra\Assertion\AssertionManager;
 use ZfExtra\Config\Config;
-use ZfExtra\Console\CommandManager;
-use ZfExtra\ModuleManager\Feature\AssertionProviderInterface;
-use ZfExtra\ModuleManager\Feature\CommandProviderInterface;
-use const DEBUG;
 
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
-
-    public function init(ModuleManager $moduleManager)
-    {
-        /** @var ServiceLocatorInterface $sm */
-//        $sm = $moduleManager->getEvent()->getParam('ServiceManager');
-//
-//        /** @var ServiceListenerInterface $serviceListener */
-//        $serviceListener = $sm->get('ServiceListener');
-//
-//        $serviceListener->addServiceManager(CommandManager::class, 'config_key', CommandProviderInterface::class, 'getCommandsConfig');
-//        $serviceListener->addServiceManager(AssertionManager::class, 'assertions', AssertionProviderInterface::class, 'getAssertionsConfig');
-    }
 
     public function onBootstrap(MvcEvent $event)
     {
