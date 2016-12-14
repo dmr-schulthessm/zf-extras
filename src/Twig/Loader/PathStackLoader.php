d1227aaaa13ae0df8cb189aed6c9932da6295892
Marc Schulthess <marc.schulthess@mrutschmann.com>
2016-12-14 16:08:03 +0100

    fix deprecated twig function

--- src/Twig/Loader/PathStackLoader.php
+++ src/Twig/Loader/PathStackLoader.php
@@ -42,6 +42,7 @@ class PathStackLoader extends Twig_Loader_Filesystem
 
     protected function findTemplate($name)
     {
+        $throw = func_num_args() > 1 ? func_get_arg(1) : true;
         $name = (string) $name;
         // normalize name
         $name = preg_replace('#/{2,}#', '/', strtr($name, '\\', '/'));
@@ -58,12 +59,20 @@ class PathStackLoader extends Twig_Loader_Filesystem
         $namespace = '__main__';
         if (isset($name[0]) && '@' == $name[0]) {
             if (false === $pos = strpos($name, '/')) {
+                if (!$throw) {
+                    return false;
+                }
+
                 throw new Twig_Error_Loader(sprintf('Malformed namespaced template name "%s" (expecting "@namespace/template_name").', $name));
             }
             $namespace = substr($name, 1, $pos - 1);
             $name = substr($name, $pos + 1);
         }
         if (!isset($this->paths[$namespace])) {
+            if (!$throw) {
+                return false;
+            }
+
             throw new Twig_Error_Loader(sprintf('There are no registered paths for namespace "%s".', $namespace));
         }
         foreach ($this->paths[$namespace] as $path) {
@@ -71,6 +80,11 @@ class PathStackLoader extends Twig_Loader_Filesystem
                 return $this->cache[$name] = $path . '/' . $name;
             }
         }
+
+        if (!$throw) {
+            return false;
+        }
+
         throw new Twig_Error_Loader(sprintf('Unable to find template "%s" (looked into: %s).', $name, implode(', ', $this->paths[$namespace])));
     }
 
